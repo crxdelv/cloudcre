e92c4c209d216daae101d88af4b3fd8270f87431
si delv <151720755+creuserr@users.noreply.github.com>
2024-06-18 21:03:39 +0800

    update index.js

--- index.js
+++ index.js
@@ -376,7 +376,7 @@ function purgeCache() {
 
 $(document).ready(async _ => {
   cc_trending = await getTrending();
-  const seg = location.pathname.slice(1).split("/");
+  const seg = location.pathname.slice(1).split("/").map(i => decodeURIComponent(seg));
   if(seg.length == 4) {
     await openTrack(seg[2], seg[3], seg[0], seg[1]);
   }
